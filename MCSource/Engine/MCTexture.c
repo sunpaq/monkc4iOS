204e0a3a3c50c898e9beb3502c96369baf85a99e
oreisoft <oreisoft@yeah.net>
2015-12-25 00:54:12 +0800

    test simple texture data, add libSOIL

--- MCSource/Engine/MCTexture.c
+++ MCSource/Engine/MCTexture.c
@@ -8,6 +8,7 @@
 
 #include "MCTexture.h"
 #include "MC3DBase.h"
+//#include "external/SOIL.h"
 
 static const GLfloat texCoor[] = {
     //vertex position
@@ -17,14 +18,41 @@ static const GLfloat texCoor[] = {
     -0.5f,  0.5f, 0.0f,
     //tex coordinate
     0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
     1.0f, 0.0f,
+    1.0f, 0.0f,
+    0.0f, 0.0f,
 };
+
+static void blackWhiteCheckerBoardTexture()
+{
+    // Black/white checkerboard
+    float pixels[] = {
+        0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
+    };
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+}
+
+//static void prepareTexture(const char* path)
+//{
+//    int width, height;
+//    unsigned char* image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
+//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+//    SOIL_free_image_data(image);
+//}
 
 oninit(MCTexture)
 {
-    var(textureId) = MCLoadSpriteTexture("mcicon", "png");
+    //var(textureId) = MCLoadSpriteTexture("mcicon", "png");
+    //prepareTexture(MCFileGetPath("mcicon", "png"));
+    
+    glGenTextures(1, &(var(textureId)));
+    glBindTexture(GL_TEXTURE_2D, var(textureId));
+    glEnable(GL_TEXTURE_2D);
+    
+    blackWhiteCheckerBoardTexture();
+
+
     
     glGenBuffers(1, &var(bufferId));
     glBindBuffer(GL_ARRAY_BUFFER, var(bufferId));
