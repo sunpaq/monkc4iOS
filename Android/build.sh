6a55ad594718d023f15a43f470773c7ccb979dec
Sun YuLi <sunpaq@gmail.com>
2016-07-19 18:28:37 +0800

    1. add build script. 2. add gitignore for Android folder.

--- Android/build.sh
+++ Android/build.sh
@@ -2,8 +2,16 @@
 
 #you have to make sure the following tools
 NDK_BUILD='ndk-build'
-SDK_BUILD=''
+SDK_BUILD='ant'
 
+#build the native library
 cd jni
-$NDK_BUILD clean && $NDK_BUILD
+$NDK_BUILD clean
+$NDK_BUILD
 cd ..
+
+#build the Android App project
+$SDK_BUILD clean
+$SDK_BUILD debug
+
+echo 'DONE'
