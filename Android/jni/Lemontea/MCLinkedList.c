51453e40d8f5508020a8e5d15ee3bb554bd1da39
Yuli <yu.li.sun@cn.schindler.com>
2016-08-03 13:47:45 +0800

    add CArrayLinkedList

--- Android/jni/Lemontea/MCLinkedList.c
+++ Android/jni/Lemontea/MCLinkedList.c
@@ -174,3 +174,6 @@ onload(MCLinkedList)
         return mull;
     }
 }
+
+
+
--- Android/jni/MCSource/Testbed.c
+++ Android/jni/MCSource/Testbed.c
@@ -7,7 +7,7 @@
 //
 
 #include "Testbed.h"
-
+#include "MCLinkedList.h"
 //static void test(mc_message_arg(MCObject), MCChar arg1, MCInt arg2, MCGeneric arg3, MCLongLong arg4, MCPtr arg5, MCFuncPtr arg6)
 //static void testmethod(mc_message_arg(MCObject), ...)
 //{
@@ -34,8 +34,29 @@
 //    
 //}
 
+//static void testCArrayLinkedList()
+//{
+//    CArrayLinkedListItem array[5];
+//    long values[] = {123,456,789,111,222};
+//    CArrayLinkedListItem* head = initCArrayLinkedList(array, values, 5);
+//
+//    head = deleteCArrayLinkedListItem(array, &array[0]);
+//    head = deleteCArrayLinkedListItem(array, &array[1]);
+//    head = deleteCArrayLinkedListItem(array, &array[4]);
+//
+//    CArrayLinkedListItem* current = head;
+//    while (current != mull) {
+//        printf("current item value: %ld\n", current->value);
+//        current = nextCArrayLinkedListItem(array, current);
+//    }
+//    
+//    printf("test end\n");
+//    exit(-1);
+//}
+
 void starttest()
 {
     //testclass();
     //testparser();
+    //testCArrayLinkedList();
 }
